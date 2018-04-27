d97a53f18cb1621e6ad037515d88865708d3b176
ihack4falafel <hashem.jawad@yahoo.com>
2018-04-26 22:42:45 -0400

    Code optimazation and bug fixes!

--- Slink.py
+++ Slink.py
@@ -24,8 +24,8 @@
 import sys
 import time
 
-# This is an alternative encoder function if [01] is found
-def AltEncoder1(item, FirstAdd, SecondAdd, ThirdAdd):
+# This is an alternative encoder function if [01] and/or [f] found
+def AltEncoder(item, FirstAdd, SecondAdd, ThirdAdd):
 	for i in list(item):
 		if i == '0':
 			FirstAdd   += "1"
@@ -89,7 +89,7 @@ def AltEncoder1(item, FirstAdd, SecondAdd, ThirdAdd):
 			ThirdAdd   += "6"
 		elif i == 'f':
 			FirstAdd   += "7"
-    			SecondAdd  += "4"
+    			SecondAdd  += "5"
 			ThirdAdd   += "5"
 	print 'and eax, 0x10101010'
 	print 'and eax, 0x01010101'
@@ -97,80 +97,6 @@ def AltEncoder1(item, FirstAdd, SecondAdd, ThirdAdd):
 	print 'add eax, 0x' + SecondAdd
 	print 'add eax, 0x' + ThirdAdd
 	print 'sub eax, 0x22222222'
-	print 'push eax'
-
-# This is an alternative encoder function if [f] is found
-def AltEncoder2(item, FirstAdd, SecondAdd, ThirdAdd):
-	for i in list(item):
-		if i == '0':
-			FirstAdd   += "0"
-			SecondAdd  += "0"
-			ThirdAdd   += "0"
-		elif i == '1':
-			FirstAdd   += "1"
-			SecondAdd  += "0"
-			ThirdAdd   += "0"
-		elif i == '2':
-			FirstAdd   += "1"
-			SecondAdd  += "1"
-			ThirdAdd   += "0"
-		elif i == '3':
-			FirstAdd   += "1"
-			SecondAdd  += "1"
-			ThirdAdd   += "1"
-		elif i == '4':
-			FirstAdd   += "2"
-			SecondAdd  += "1"
-			ThirdAdd   += "1"
-		elif i == '5':
-			FirstAdd   += "2"
-			SecondAdd  += "2"
-			ThirdAdd   += "1"
-		elif i == '6':
-                       	FirstAdd   += "2"
-		 	SecondAdd  += "2"
-			ThirdAdd   += "2"
-		elif i == '7':
-                      	FirstAdd   += "3"
-			SecondAdd  += "3"
-			ThirdAdd   += "1"
-		elif i == '8':
-                 	FirstAdd   += "4"
-			SecondAdd  += "2"
-			ThirdAdd   += "2"
-		elif i == '9':
-                       	FirstAdd   += "3"
-			SecondAdd  += "3"
-			ThirdAdd   += "3"
-		elif i == 'a':
-                        FirstAdd   += "4"
-			SecondAdd  += "4"
-			ThirdAdd   += "2"
-		elif i == 'b':
-                       	FirstAdd   += "5"
-			SecondAdd  += "5"
-			ThirdAdd   += "1"
-		elif i == 'c':
-                       	FirstAdd   += "6"
-			SecondAdd  += "3"
-			ThirdAdd   += "3"
-		elif i == 'd':
-                       	FirstAdd   += "6"
-			SecondAdd  += "6"
-			ThirdAdd   += "1"
-		elif i == 'e':
-                       	FirstAdd   += "5"
-			SecondAdd  += "5"
-			ThirdAdd   += "4"
-		elif i == 'f':
-			FirstAdd   += "7"
-    			SecondAdd  += "7"
-			ThirdAdd   += "1"
-	print 'and eax, 0x10101010'
-	print 'and eax, 0x01010101'
-	print 'add eax, 0x' + FirstAdd
-	print 'add eax, 0x' + SecondAdd
-	print 'add eax, 0x' + ThirdAdd
 	print 'push eax'
 
 # This is default encoder function if none of 8 bytes chuncks have [f] or [01]
@@ -261,16 +187,16 @@ def main():
 		print '[' +G+ '+' +W+ '] Encoding [%s]..' %item
 		time.sleep(1)
 		TwoBytes = [item[i:i+2] for i in range(0, len(item), 2)]
-		# if this is true go to AltEncoder1
+		# if this is true go to AltEncoder
 		if any(i == '01' for i in list(TwoBytes)):
-			print '[' +R+ '!' +W+'] [' +O+ '01' +W+ '] found in shellcode, using alterantive encoder..'
+			print '[' +R+ '!' +W+'] [' +O+ '01' +W+ '] and/or [' +O+ 'f' +W+ '] found in shellcode, using alterantive encoder..'
 			time.sleep(1)
-			AltEncoder1(item, FirstAdd, SecondAdd, ThirdAdd)
-		# if this is true go to AltEncoder2
+			AltEncoder(item, FirstAdd, SecondAdd, ThirdAdd)
+		# if this is true go to AltEncoder
 		elif any(i == 'f' for i in list(item)):
-			print '[' +R+ '!' +W+ '] [' +O+ 'f' +W+ '] found in shellcode, using alterantive encoder..'
+			print '[' +R+ '!' +W+'] [' +O+ '01' +W+ '] and/or [' +O+ 'f' +W+ '] found in shellcode, using alterantive encoder..'
 			time.sleep(1)
-			AltEncoder2(item, FirstAdd, SecondAdd, ThirdAdd)
+			AltEncoder(item, FirstAdd, SecondAdd, ThirdAdd)
 		# if this is true go to DefaultEncoder
 		else:
 			print '[' +G+ '+' +W+ '] [' +O+ 'f' +W+ '] and [' +O+ '01' +W+ '] not found, using default encoder..'
